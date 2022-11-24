0e34329629566adbd5384435640968dfc952b7c5
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2022-11-23 20:56:11 -0500

    Refactor edge detection

--- fiso/edge.py
+++ fiso/edge.py
@@ -1,44 +1,166 @@
-# code for identifying the edge cells of a core. 
-
 import numpy as np
-from .fiso_periodic import shear_bcn, compute_displacement
 
-def iso_edge(iso,bi,bpcn,displacements):
-    """Do XXX
+def get_edge_cells(cells, pcn):
+    """Find edge cells of the given region
 
-    Arguments
-    ---------
-    iso: a single fiso object
-    bi: boundary index
-    bpcn: boundary precomputed neighbors
-    displacements:
+    Parameters
+    ----------
+    cells : array_like
+        flattened indices that defines some region
+    pcn : array_like
+        precomputed neighbors.
 
     Return
     ------
+    edge_cells : array_like
+        flattened indices that defines edge cells of the given region
     """
-    members = np.array(iso)
-    memlen = len(members)
-    # nli is neighbor list by index
-    nli = members[:,None] + displacements[None]
-    # correct boundary by locating the nli index if existent of each bi 
-    # (boundary index)
-    # and inserting the associated entry from bpcn 
-    # (boundary precomputed neighbors)
-    mem_as = np.argsort(members)
-    mem_as_bi = np.searchsorted(members[mem_as],bi)
-    nli[mem_as[mem_as_bi[mem_as_bi < memlen]]] = bpcn[mem_as_bi < memlen]
-    # if any neighbor isn't a member, this member is edge
-    return members[np.any(np.isin(nli,members,invert=True),axis=1)]
+    cells = np.array(cells)
+    # For each cell, are there neighboring cells which is not contained
+    # in the given region? That is, is there any element of "pcn" which is
+    # not contained in "cells"?
+    adjacent_exterior = np.isin(pcn[cells], cells, invert=True)
+    # If any of N_neighbor cells fall in exterior region, mark True.
+    edge_mask = np.any(adjacent_exterior, axis=1)
+    edge_cells = cells[edge_mask]
+    return edge_cells
 
-def make_cd_edge(cells_dict,cell_shear,shape):
-    # Initialize
-    out_cd = {}
-    # Compute bi, bpcn (boundary neighbors)
-    corner_bool = True
-    bi,bpcn = shear_bcn(shape,cell_shear)
-    displacements = compute_displacement(shape,corner_bool)
 
-    for iso in cells_dict.keys():
-        out_cd[iso] = iso_edge(cells_dict[iso],bi,bpcn,displacements)
-        # edge cells
-    return out_cd
+def precompute_neighbor(shape, corner=True, boundary_flag='periodic'):
+    """Precompute neighbor indices
+
+    Parameters
+    ----------
+    shape : tuple
+        shape of the input data
+    corner : Boolean, optional
+        If true, the corner cells are counted as neighbors (26 neighbors in total)
+    boundary_flag: str, optional
+        Flag for boundary condition. Affects how to set neighbors of the edge cells.
+
+    Returns
+    -------
+    pcn : array_like
+        The shape of this array is (Ncells, N_neighbors), such that pcn[1][0] is
+        the flattened index of the (-1,-1,-1) neighbor of the (k,j,i) = (0,0,1)
+        cell, which is (k,j,i) = (-1, -1, 0) = (Nz-1, Ny-1, 0) for periodic BC.
+        See docstring of get_offsets for the ordering of neighbor directions.
+    """
+    Ncells = np.prod(shape)
+    # Save on memory when applicable
+    if Ncells < 2**31:
+        dtype = np.int32
+    else:
+        dtype = np.int64
+    # Set up offset array
+    dim = len(shape)
+    offsets = _get_offsets(dim, corner)
+    # Set up displacements in index space (treat n-d array as 1-d list)
+    # factor = (NxNy, Nx, 1)
+    factor = np.cumprod(np.append([1],shape[::-1]))[:-1][::-1]
+    factor = factor.astype(dtype)
+    displacements = (offsets*factor).sum(axis=1)
+    # Displacements is num_neighbors 1-d array
+    indices = np.arange(Ncells, dtype=dtype)[:,None]
+    # indices is 1-d array, 1 for each cell
+    pcn = indices + displacements[None]
+    # pcn is 2-d array using :,None to combine
+    # Apply boundary correction
+    bi,bpcn = _boundary_i_bcn(shape, dtype, offsets, corner, boundary_flag)
+    # pcn shape num_neighbors x cells
+    # bcn shape num_neighbors x cells
+    pcn[bi] = bpcn
+    return pcn
+
+
+def _get_offsets(dim, corner=True):
+    """Compute 1-D flattened array offsets corresponding to neighbors
+
+    Parameters
+    ----------
+    dim : int
+        dimension of the input data
+    corner : Boolean, optional
+        If true, the corner cells are counted as neighbors (26 neighbors in total)
+
+    Returns
+    -------
+    offsets : array-like
+        The shape of this array is (N_neighbors, dim). Each rows are integer
+        directions that points to the neighbor cells. For example, in 3D, the
+        offsets will look like:
+        (-1, -1, -1)
+        (-1, -1,  0)
+        (-1, -1,  1)
+        (-1,  0, -1)
+        (-1,  0,  0)
+        (-1,  0,  1)
+        (-1,  1, -1)
+        (-1,  1,  0)
+        (-1,  1,  1)
+        ( 0, -1, -1)
+        ...
+    """
+    offs = [-1,0,1]
+    offsets = list(itertools.product(offs,repeat=dim))
+    if corner:
+        offsets.remove((0,)*dim)
+    else:
+        offsets = [i for i in offsets if i.count(0) == 2]
+    offsets = np.array(offsets, dtype=np.int32)
+    return offsets
+
+
+def _boundary_i_bcn(shape, dtype, offsets, corner, boundary_flag):
+    # returns boundary indices and boundary's neighbor indices.
+    boundary_indices = _gbi(shape,dtype)
+    boundary_coords = np.array(np.unravel_index(boundary_indices,shape),
+                              dtype=dtype)
+    bpcn = _boundary_pcn(boundary_coords, offsets, shape, corner,
+                        boundary_flag=boundary_flag).astype(dtype)
+    return boundary_indices,bpcn
+
+
+def _boundary_pcn(coords, offsets, shape, corner, boundary_flag='periodic'):
+    if boundary_flag=='periodic':
+        mode='wrap'
+    else:
+        mode='clip'
+    newcoords = coords[:,:,None] + np.transpose(offsets)[:,None,:]
+    output = np.ravel_multi_index(newcoords,shape,mode=mode)
+    return output
+
+
+def _gbi(shape, dtype):
+    """get boundary indices from shape"""
+    shape = list(shape)
+    bi = []
+    ls = len(shape)
+    basel = ls*[None] #array slice none to extend array dimension
+    idx = range(ls) #index for loops
+    #dni is the coords for dimension "i"
+    dni = ls*[None]
+    for i in idx:
+        dni[i] = np.arange(shape[i],dtype=dtype)
+
+    #for boundary dimensions i set indices j != i, setting index i to be
+    #0 or end
+    for i in idx:
+
+        ndnis = ls*[None]
+        shapei = shape[:] #copy shape
+        shapei[i] = 1     #set dimension i to 1 (flat boundary)
+        nzs = np.zeros(shapei,dtype=dtype) #initialize boundary to 0
+        for j in idx:
+            if j == i:
+                continue
+                #make coord j using the np.arange (dni) with nzs of desired shape
+            selj = basel[:]
+            selj[j] = slice(None)
+            #slicing on index j makes dni[j] vary on index j and copy on other dimensions with desired shape nzs
+            ndnis[j] = dni[j][tuple(selj)] + nzs
+        ndnis[i] = 0
+        bi += list(np.ravel_multi_index(ndnis,shape).flatten())
+        ndnis[i] = shape[i]-1
+        bi += list(np.ravel_multi_index(ndnis,shape).flatten())
+    return bi
--- fiso/fiso.py
+++ fiso/fiso.py
@@ -4,6 +4,7 @@ import time
 from itertools import islice
 import itertools
 from collections import deque
+from .edge import precompute_neighbor
 
 # printing extra diagnostic messages
 verbose = True
@@ -12,6 +13,7 @@ boundary_flag = 'periodic'
 # whether diagonal cells are neighbors
 time.prevtime = time.time()
 
+
 def timer(string=''):
     thistime = time.time()
     dt = thistime - time.prevtime
@@ -19,6 +21,7 @@ def timer(string=''):
     if len(string) > 0:
         if verbose: print(string,str(dt) + " seconds elapsed")
     return dt
+
 
 def setup(data):
     timer()
@@ -37,28 +40,6 @@ def setup(data):
     if verbose:
         print("Found {} minima".format(len(idx_minima)))
     return idx_minima, order, len(order), pcn
-
-
-def find_minima_no_bc(arr):
-    '''
-    Find minima using sn, don't allow any boundary cells to be minima
-    Then add the boundaries
-    '''
-    # initially doesnt allow any boundary cells to be minima
-    local_min = (arr == minimum_filter(arr, size=3, mode='constant',
-                                       cval=-np.inf)).flatten()
-    return local_min
-
-def find_minima_boundary_only(data_flat,indices,bcn):
-    '''
-    data_flat: 1-d array data
-    indices: 1-d array of boundary flattened indices
-    bcn: boundary neighbors: num_indices x num_neighbors 2-d array of
-    flattened indices
-    output: indices that are local minima.
-    '''
-    indices = np.array(indices)
-    return indices[data_flat[indices] <= np.min(data_flat[bcn],axis=1)]
 
 
 def find_minima_global(arr, boundary_flag='periodic'):
@@ -87,179 +68,24 @@ def find_minima_global(arr, boundary_flag='periodic'):
     return local_min
 
 
-def boundary_pcn(coords, offsets, shape, corner, boundary_flag='periodic'):
-    if boundary_flag=='periodic':
-        mode='wrap'
-    else:
-        mode='clip'
-    newcoords = coords[:,:,None] + np.transpose(offsets)[:,None,:]
-    output = np.ravel_multi_index(newcoords,shape,mode=mode)
-    return output
-    # start with coords of shape dim,num_coords
-    # for each num_coords, add one of the many offsets
-    # offsets has shape num_neighbors,dim
-    # np.transpose(offsets)
-    # dim, num_neighbors
-    # want something of shape dim,num_coords,num_neighbors
-    # num_coords, num_neighbors
-
-def gbi_axis(shape,dtype,axis):
+def find_minima_no_bc(arr):
     '''
-    get boundary indices of axis from shape
-    useful when boundary condition is axis specific
+    Find minima using sn, don't allow any boundary cells to be minima
+    Then add the boundaries
     '''
-    shape = list(shape)
-    dim = len(shape)
-    basel = dim*[None] #array slice none to extend array dimension
-    idx = range(dim) #index for loops
-    #dni is the coords for dimension "i"
-    dni = dim*[None]
-    for i in idx:
-        dni[i] = np.arange(shape[i],dtype=dtype)
-    #for boundary dimensions i set indices j != i, setting index i to be
-    #0 or end
+    # initially doesnt allow any boundary cells to be minima
+    local_min = (arr == minimum_filter(arr, size=3, mode='constant',
+                                       cval=-np.inf)).flatten()
+    return local_min
 
-    ndnis = dim*[None]
-    i = axis
-    shapei = shape[:] #copy shape
-    shapei[i] = 1     #set dimension i to 1 (flat boundary)
-    nzs = np.zeros(shapei,dtype=dtype) #initialize boundary to 0
-    for j in idx:
-        if j == i:
-            continue
-        #make coord j using the np.arange (dni) with nzs of desired shape
-        selj = basel[:]
-        selj[j] = slice(None)
-        #slicing on index j makes dni[j] vary on index j and copy on other dimensions with desired shape nzs
-        ndnis[j] = dni[j][tuple(selj)] + nzs
-    ndnis[i] = 0
-    face0 = list(np.ravel_multi_index(ndnis,shape).flatten())
-    ndnis[i] = shape[i]-1
-    face1 = list(np.ravel_multi_index(ndnis,shape).flatten())
-    return face0,face1
 
-def gbi(shape, dtype):
-    """get boundary indices from shape"""
-    shape = list(shape)
-    bi = []
-    ls = len(shape)
-    basel = ls*[None] #array slice none to extend array dimension
-    idx = range(ls) #index for loops
-    #dni is the coords for dimension "i"
-    dni = ls*[None]
-    for i in idx:
-        dni[i] = np.arange(shape[i],dtype=dtype)
-
-    #for boundary dimensions i set indices j != i, setting index i to be
-    #0 or end
-    for i in idx:
-
-        ndnis = ls*[None]
-        shapei = shape[:] #copy shape
-        shapei[i] = 1     #set dimension i to 1 (flat boundary)
-        nzs = np.zeros(shapei,dtype=dtype) #initialize boundary to 0
-        for j in idx:
-            if j == i:
-                continue
-                #make coord j using the np.arange (dni) with nzs of desired shape
-            selj = basel[:]
-            selj[j] = slice(None)
-            #slicing on index j makes dni[j] vary on index j and copy on other dimensions with desired shape nzs
-            ndnis[j] = dni[j][tuple(selj)] + nzs
-        ndnis[i] = 0
-        bi += list(np.ravel_multi_index(ndnis,shape).flatten())
-        ndnis[i] = shape[i]-1
-        bi += list(np.ravel_multi_index(ndnis,shape).flatten())
-    return bi
-
-def get_offsets(dim, corner=True):
-    """Compute 1-D flattened array offsets corresponding to neighbors
-
-    Parameters
-    ----------
-    dim : int
-        dimension of the input data
-    corner : Boolean, optional
-        If true, the corner cells are counted as neighbors (26 neighbors in total)
-
-    Returns
-    -------
-    offsets : array-like
-        The shape of this array is (N_neighbors, dim). Each rows are integer
-        directions that points to the neighbor cells. For example, in 3D, the
-        offsets will look like:
-        (-1, -1, -1)
-        (-1, -1,  0)
-        (-1, -1,  1)
-        (-1,  0, -1)
-        (-1,  0,  0)
-        (-1,  0,  1)
-        (-1,  1, -1)
-        (-1,  1,  0)
-        (-1,  1,  1)
-        ( 0, -1, -1)
-        ...
-    """
-    offs = [-1,0,1]
-    offsets = list(itertools.product(offs,repeat=dim))
-    if corner:
-        offsets.remove((0,)*dim)
-    else:
-        offsets = [i for i in offsets if i.count(0) == 2]
-    offsets = np.array(offsets, dtype=np.int32)
-    return offsets
-
-def precompute_neighbor(shape,corner=True,boundary_flag='periodic'):
-    """Precompute neighbor indices
-
-    Parameters
-    ----------
-    shape : tuple
-        shape of the input data
-    corner : Boolean, optional
-        If true, the corner cells are counted as neighbors (26 neighbors in total)
-    boundary_flag: str, optional
-        Flag for boundary condition. Affects how to set neighbors of the edge cells.
-
-    Returns
-    -------
-    pcn : array_like
-        The shape of this array is (Ncells, N_neighbors), such that pcn[1][0] is
-        the flattened index of the (-1,-1,-1) neighbor of the (k,j,i) = (0,0,1)
-        cell, which is (k,j,i) = (-1, -1, 0) = (Nz-1, Ny-1, 0) for periodic BC.
-        See docstring of get_offsets for the ordering of neighbor directions. 
-    """
-    Ncells = np.prod(shape)
-    # Save on memory when applicable
-    if Ncells < 2**31:
-        dtype = np.int32
-    else:
-        dtype = np.int64
-    # Set up offset array
-    dim = len(shape)
-    offsets = get_offsets(dim, corner)
-    # Set up displacements in index space (treat n-d array as 1-d list)
-    # factor = (NxNy, Nx, 1)
-    factor = np.cumprod(np.append([1],shape[::-1]))[:-1][::-1]
-    factor = factor.astype(dtype)
-    displacements = (offsets*factor).sum(axis=1)
-    # Displacements is num_neighbors 1-d array
-    indices = np.arange(Ncells, dtype=dtype)[:,None]
-    # indices is 1-d array, 1 for each cell
-    pcn = indices + displacements[None]
-    # pcn is 2-d array using :,None to combine
-    # Apply boundary correction
-    bi,bpcn = boundary_i_bcn(shape, dtype, offsets, corner, boundary_flag)
-    # pcn shape num_neighbors x cells
-    # bcn shape num_neighbors x cells
-    pcn[bi] = bpcn
-    return pcn
-    
-def boundary_i_bcn(shape, dtype, offsets, corner, boundary_flag):
-    # returns boundary indices and boundary's neighbor indices.
-    boundary_indices = gbi(shape,dtype)
-    boundary_coords = np.array(np.unravel_index(boundary_indices,shape),
-                              dtype=dtype)
-    bpcn = boundary_pcn(boundary_coords, offsets, shape, corner,
-                        boundary_flag=boundary_flag).astype(dtype)
-    return boundary_indices,bpcn
+def find_minima_boundary_only(data_flat,indices,bcn):
+    '''
+    data_flat: 1-d array data
+    indices: 1-d array of boundary flattened indices
+    bcn: boundary neighbors: num_indices x num_neighbors 2-d array of
+    flattened indices
+    output: indices that are local minima.
+    '''
+    indices = np.array(indices)
+    return indices[data_flat[indices] <= np.min(data_flat[bcn],axis=1)]
